fcf23cd05ddeefe7c3c5659b7101f91428191ae7
NaviteLogger5547 <66165957+NaviteLogger@users.noreply.github.com>
2022-12-17 19:16:09 +0100

    Finished working on the static list

---
+++ Test.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using StaticList;
+
+public class Program
+{
+	static void Main()
+	{
+        StaticList.CustomArrayList<int> tablica = new CustomArrayList<int>(10);
+		
+        Console.WriteLine("It works!");
+
+		for (int j = 0; j < 15; j++)
+		{
+			tablica.Add(j+1);
+			Console.WriteLine(tablica[j]);
+		}
+	}
+	
+}
