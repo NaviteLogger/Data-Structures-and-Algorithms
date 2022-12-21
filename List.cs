3e4d6b17c9b348a4988ec53c025a2873a2f94c8c
NaviteLogger5547 <66165957+NaviteLogger@users.noreply.github.com>
2022-12-22 00:37:17 +0100

    Added a tons of test to main

--- List.cs
+++ List.cs
@@ -6,8 +6,70 @@ using System.Threading.Tasks;
 
 namespace List
 {
-    public class List<T> : System.Collections.Generic.List<T>
+    public class InheritedList<T> : System.Collections.Generic.List<T>
     {
         //all necessary elements will be inherited from System.Collections.Generic.List<T>
-    }
+
+        public static List<int> GetPrimes(int start, int end)
+        {
+            List<int> primesList = new List<int>();
+            for (int num = start; num <= end; num++)
+            {
+                bool prime = true;
+                double numSqrt = Math.Sqrt(num);
+                for (int div = 2; div <= numSqrt; div++)
+                {
+                    if (num % div == 0)
+                    {
+                        prime = false;
+                        break;
+                    }
+                }
+                if (prime)
+                {
+                    primesList.Add(num);
+                }
+            }
+            return primesList;
+        }
+
+        public static List<int> Union(List<int> firstList, List<int> secondList)
+        {
+            List<int> union = new List<int>();
+            union.AddRange(firstList);
+            foreach (var item in secondList)
+            {
+                if (!union.Contains(item))
+                {
+                    union.Add(item);
+                }
+            }
+            return union;
+        }
+        
+        public static List<int> Intersect(List<int>firstList, List<int> secondList)
+        {
+            List<int> intersect = new List<int>();
+            foreach (var item in firstList)
+            {
+                if (secondList.Contains(item))
+                {
+                    intersect.Add(item);
+                }
+            }
+            return intersect;
+        }
+        
+        public static void PrintList(List<int> list)
+        {
+            Console.Write("{ ");
+            foreach (var item in list)
+            {
+                Console.Write(item);
+                Console.Write(" ");
+            }
+            Console.WriteLine("}");
+        }
+    }    
 }
+
--- Test.cs
+++ Test.cs
@@ -3,6 +3,9 @@ using System.Runtime.InteropServices;
 using StaticList;
 using LinkedList;
 using DoublyLinkedList;
+using List;
+using static DSA.Trees_and_Graphs.TreeImplementation;
+using System.Collections;
 
 public class Program
 {
@@ -10,16 +13,194 @@ public class Program
 	{
         StaticList.StaticList<int> tablica = new StaticList<int>(10);
 
-/*		for (int j = 0; j < 15; j++)
-		{
-			tablica.Add(j+1);
-			Console.WriteLine(tablica[j]);
-		}*/
+        for (int j = 0; j < 15; j++)
+        {
+            tablica.Add(j + 1);
+            Console.WriteLine(tablica[j]);
+        }
 
         LinkedList.DynamicList<int> lista = new DynamicList<int>();
         DoublyLinkedList.LinkedList<int> listaDynamiczna = new DoublyLinkedList.LinkedList<int>();
 
         Console.WriteLine("It works!");
+
+        //playing with ArrayList 
+        ArrayList ArrayList = new ArrayList();
+        ArrayList.Add("Hello");
+        ArrayList.Add(5);
+        ArrayList.Add(3.14159);
+        ArrayList.Add(DateTime.Now);
+        for (int i = 0; i < ArrayList.Count; i++)
+        {
+#pragma warning disable CS8600 // Converting null literal or possible null value to non-nullable type.
+            object value = ArrayList[i];
+#pragma warning restore CS8600 // Converting null literal or possible null value to non-nullable type.
+            Console.WriteLine("Index={0}; Value={1}", i, value);
+        }
+
+        ArrayList NumerList = new ArrayList();
+        NumerList.Add(2);
+        NumerList.Add(3.5f);
+        NumerList.Add(25u);
+        NumerList.Add(" EUR");
+        dynamic sum = 0;
+        for (int i = 0; i < NumerList.Count; i++)
+        {
+#pragma warning disable CS8600 // Converting null literal or possible null value to non-nullable type.
+            dynamic value = NumerList[i];
+#pragma warning restore CS8600 // Converting null literal or possible null value to non-nullable type.
+            sum = sum + value;
+        }
+        Console.WriteLine("Sum = " + sum);
+
+        //playing with the lists
+
+        List<int> primes =  InheritedList<int>.GetPrimes(200, 300);
+        foreach (var item in primes)
+        {
+            Console.Write("{0} ", item);
+        }
+
+        
+        InheritedList<int> firstList = new InheritedList<int>();
+        firstList.Add(1);
+        firstList.Add(2);
+        firstList.Add(3);
+        firstList.Add(4);
+        firstList.Add(5);
+        Console.Write("firstList = ");
+        InheritedList<int>.PrintList(firstList);
+        
+        List<int> secondList = new List<int>();
+        secondList.Add(2);
+        secondList.Add(4);
+        secondList.Add(6);
+        Console.Write("secondList = ");
+        InheritedList<int>.PrintList(secondList);
+        
+        List<int> unionList = new List<int>();
+        unionList.AddRange(firstList);
+        for (int i = unionList.Count - 1; i >= 0; i--)
+        {
+            if (secondList.Contains(unionList[i]))
+            {
+                unionList.RemoveAt(i);
+            }
+        }
+        unionList.AddRange(secondList);
+        Console.Write("union = ");
+        InheritedList<int>.PrintList(unionList);
+        
+        List<int> intersectList = new List<int>();
+        intersectList.AddRange(firstList);
+        for (int i = intersectList.Count - 1; i >= 0; i--)
+        {
+            if (!secondList.Contains(intersectList[i]))
+            {
+                intersectList.RemoveAt(i);
+            }
+        }
+        Console.Write("intersect = ");
+        InheritedList<int>.PrintList(intersectList);
+
+
+        //converting a list to array and back
+        int[] arr = new int[] { 1, 2, 3 };
+        List<int> list = new List<int>(arr);
+        int[] convertedArray = list.ToArray();
+        
+
+        //playing with stacks
+        Stack<string> stack = new Stack<string>();
+        stack.Push("1. John");
+        stack.Push("2. Nicolas");
+        stack.Push("3. Mary");
+        stack.Push("4. George");
+        Console.WriteLine("Top = " + stack.Peek());
+        while (stack.Count > 0)
+        {
+            string personName = stack.Pop();
+            Console.WriteLine(personName);
+        }
+
+        //brackets check example
+        string expression = "1 + (3 + 2 - (2+3)*4 - ((3+1)*(4-2)))";
+        Stack<int> BracketsCheckStack = new Stack<int>();
+        bool correctBrackets = true;
+        for (int BracketsCheckIndex = 0; BracketsCheckIndex < expression.Length; BracketsCheckIndex++)
+        {
+            char ch = expression[BracketsCheckIndex];
+            if (ch == '(')
+            {
+                BracketsCheckStack.Push(BracketsCheckIndex);
+            }
+            else if (ch == ')')
+            {
+                if (BracketsCheckStack.Count == 0)
+                {
+                    correctBrackets = false;
+                    break;
+                }
+                BracketsCheckStack.Pop();
+            }
+        }
+        if (BracketsCheckStack.Count != 0)
+        {
+            correctBrackets = false;
+        }
+        Console.WriteLine("Are the brackets correct? " +correctBrackets);
+
+        //playing with queues
+        Queue<string> queue = new Queue<string>();
+        queue.Enqueue("Message One");
+        queue.Enqueue("Message Two");
+        queue.Enqueue("Message Three");
+        queue.Enqueue("Message Four");
+        while (queue.Count > 0)
+        {
+            string msg = queue.Dequeue();
+            Console.WriteLine(msg);
+        }
+
+
+        //a sequene using a queue
+        int n = 3;
+        int p = 16;
+        Queue<int> queue2 = new Queue<int>();
+        queue2.Enqueue(n);
+        int index = 0;
+        Console.WriteLine("S =");
+        while (queue.Count > 0)
+        {
+            index++;
+            int current = queue2.Dequeue();
+            Console.WriteLine(" " + current);
+            if (current == p)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Index = " + index);
+                return;
+            }
+            queue2.Enqueue(current + 1);
+            queue2.Enqueue(2 * current);
+        }
+        
+       
+        
+        // Create the tree from the sample
+        Tree<int> tree =
+        new Tree<int>(7,
+        new Tree<int>(19,
+        new Tree<int>(1),
+        new Tree<int>(12),
+        new Tree<int>(31)),
+        new Tree<int>(21),
+        new Tree<int>(14,
+        new Tree<int>(23),
+        new Tree<int>(6))
+        );
+        // Traverse and print the tree using Depth-First-Search
+        tree.TraverseDFS();
     }
 	
 }
