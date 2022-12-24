95eac00259784b465ef6be6f1cf0c4bf5d73f40b
NaviteLogger5547 <66165957+NaviteLogger@users.noreply.github.com>
2022-12-24 11:35:34 +0100

    Added the dictionary class

---
+++ Dictionaries, Hash-tables and Sets/Dictionary.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.Dictionaries__Hash_tables_and_Sets
+{
+    internal class Dictionary
+    {
+    }
+}
